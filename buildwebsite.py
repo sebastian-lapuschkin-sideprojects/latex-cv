20cf4ab178f538e96993e2b4f4ce7df4f00f258c
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2021-06-28 22:09:51 +0200

    extending build code

--- buildwebsite.py
+++ buildwebsite.py
@@ -1,9 +1,14 @@
+import shutil
+
 template = './resources/vcard_template.htm'
 vcard = './resources/vcard.svg'
+vcard_raw = './resources/vcard_raw.svg'
 output = './resources/vcard.htm'
 
 template_str = None
 vcard_str = None
+
+shutil.copyfile(vcard_raw, vcard)
 
 with open(template, 'rt') as ft:
     template_str = ft.read()
