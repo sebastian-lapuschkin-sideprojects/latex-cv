0397186831683afcc6bcb2a2d26e16e896eaccd2
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2021-08-12 17:29:16 +0200

    reverting preview generation message.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "building cv.pdf"
 pdflatex cv &> /dev/null
 
-echo "generating png preview from pdf"
+echo "generating jpg preview from pdf"
 convert -density 200 -append cv.pdf -quality 85 preview/cv.jpg  &> /dev/null
 
 echo "removing temporary files"
