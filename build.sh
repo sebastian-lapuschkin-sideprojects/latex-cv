6c2f37d84709c3a9bddb2123df38a779fed8d222
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2023-01-27 13:58:57 +0100

    verbosity++ in build script

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ pdflatex cv &> /dev/null
 echo "generating jpg preview from pdf"
 convert -density 200 -append cv.pdf -quality 85 preview/cv.jpg  &> /dev/null
 
+echo "generated file sizes"
+du -sh cv.pdf
+du -sh preview/cv.jpg
 echo "removing temporary files"
 #removes left-over temporary build files
 bash clean.sh &> /dev/null
