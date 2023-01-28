98ee9799488e766430fb3d6c5bbbf6664006549a
sebastian-lapuschkin-sideprojects <sebastian.lapuschkin@gmail.com>
2023-01-28 17:42:01 +0100

    updating printed messages

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "building cv.pdf"
 pdflatex cv &> /dev/null
 
-echo "generating jpg preview from pdf"
+echo "generating png preview from pdf"
 convert -density 200 -append cv.pdf -quality 85 -alpha remove -alpha off preview/cv.png   &> /dev/null
 
 echo "generated file sizes"
