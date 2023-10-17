212698d30ece27ae08b4355fe34bdd319f76807e
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2023-10-17 14:55:38 +0200

    heavy refactor to enable extraction of short cv

--- build.sh
+++ build.sh
@@ -4,13 +4,24 @@
 echo "building cv.pdf"
 pdflatex cv &> /dev/null
 
-echo "generating png preview from pdf"
+#creates cv-short.pdf
+echo "building cv-short.pdf"
+pdflatex -jobname cv-short "\def\shortcv{1} \input{cv.tex}"  &> /dev/null
+
+echo "generating png preview from cv.pdf"
 convert -density 200 -append cv.pdf -quality 85 -alpha remove -alpha off preview/cv.png   &> /dev/null
 
+echo "generating png preview from cv-short.pdf"
+convert -density 200 -append cv-short.pdf -quality 85 -alpha remove -alpha off preview/cv-short.png   &> /dev/null
+
+
+
+
+
 echo "generated file sizes"
-du -sh cv.pdf
-du -sh preview/cv.png
+du -sh cv*.pdf
+du -sh preview/cv*.png
+
 echo "removing temporary files"
 #removes left-over temporary build files
 bash clean.sh &> /dev/null
-
