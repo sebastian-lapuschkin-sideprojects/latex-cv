9f2c44f4701fb19ad6be75b7d94932f3c732eb68
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2025-01-17 23:10:58 +0100

    rebuilding. we now have an extra publications list

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#creates list of publications as standalone file
+echo "building publications list"
+pdflatex -jobname cv "\def\publicationsextra{1} \input{cv.tex}" &> /dev/null
+pdftk cv.pdf cat 7-end output publications.pdf
+
 #creates cv.pdf
-echo "building cv.pdf"
+echo "building cv.pdf proper"
 pdflatex cv &> /dev/null
 
 #creates cv-short.pdf
@@ -9,18 +14,19 @@ echo "building cv-short.pdf"
 pdflatex -jobname cv-short "\def\shortcv{1} \input{cv.tex}"  &> /dev/null
 
 
-
-# generates png previews of long and short CV
+# generates png previews of long and short CV as well as publications list.
 echo "generating png preview from cv.pdf"
 convert -density 200 -append cv.pdf -quality 85 -alpha remove -alpha off preview/cv.png   &> /dev/null
 
 echo "generating png preview from cv-short.pdf"
 convert -density 200 -append cv-short.pdf -quality 85 -alpha remove -alpha off preview/cv-short.png   &> /dev/null
 
+echo "generating png preview from publications.pdf"
+convert -density 200 -append publications.pdf -quality 85 -alpha remove -alpha off preview/publications.png &> /dev/null
 
 
 echo "file sizes"
-du -sh cv*.pdf
+du -sh *.pdf
 du -sh preview/cv*.png
 
 echo "removing temporary files"
